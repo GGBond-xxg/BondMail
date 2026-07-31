3a969ded9053a958cdb8d93ee153a720e294b115
GGBond-xxg <huuyjn@163.com>
2026-07-31 23:37:42 +0800

    release: bump BondMail to v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bond.mail"
         minSdk = 26
         targetSdk = 36
-        versionCode = 100
-        versionName = "1.0.0"
+        versionCode = 101
+        versionName = "1.0.1"
         vectorDrawables.useSupportLibrary = true
     }
 
