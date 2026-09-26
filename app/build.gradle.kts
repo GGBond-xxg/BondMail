fc8e25249a2ffbf8f8fe6514eb8a8eba4e250a42
GGBond-xxg <huuyjn@163.com>
2026-09-27 00:59:35 +0800

    Release BondMail v1.5.7 (165)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.bond.mail"
         minSdk = 26
         targetSdk = 36
-        versionCode = 164
-        versionName = "1.5.6.2"
+        versionCode = 165
+        versionName = "1.5.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
